4908ee7af9a94a5b9d21fd0e252abba302149dc9
Matthew Michilot <matthew.michilot@gmail.com>
2025-09-11 10:50:02 +0000

    Update tests

--- test/oss-cad-suite/activateOnLogin_false.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source dev-container-features-test-lib
-
-check "oss-cad-suite environment is not activated" bash -c "[[ -z '${VIRTUAL_ENV_PROMPT}' ]]"
-check "motd exists" bash -c "[[ -f '/etc/update-motd.d/00-activate-suite' ]]"
-
-reportResults
--- test/oss-cad-suite/test.sh
+++ test/oss-cad-suite/test.sh
@@ -2,6 +2,7 @@
 
 source dev-container-features-test-lib
 
-check "oss-cad-suite environment is activated" bash -c "[[ '${VIRTUAL_ENV_PROMPT}' == 'OSS CAD Suite' ]]"
+check "oss-cad-suite environment is not activated" bash -c "[[ -z '${VIRTUAL_ENV_PROMPT}' ]]"
+check "check activate-suite alias" alias | grep 'activate-suite'
 
 reportResults
