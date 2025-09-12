3b9cb4e43cd89076616276d95b8b4281f7549e6d
Matthew Michilot <matthew.michilot@gmail.com>
2025-09-12 08:46:17 +0000

    Update command to retrieve archive url

--- src/oss-cad-suite/install.sh
+++ src/oss-cad-suite/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ARCHIVE_URL=$(curl -L https://api.github.com/repos/YosysHQ/oss-cad-suite-build/releases/latest | \
-    jq -r '.assets[] | select(.name | startswith("oss-cad-suite-linux-x64")) | .browser_download_url')
+    jq -r '.assets[] | select(.name | contains("linux-x64")) | .browser_download_url')
 
 wget ${ARCHIVE_URL} -O oss-cad-suite.tgz \
 && tar -xvf oss-cad-suite.tgz -C $(dirname "${SUITE_DIR}")
