d92f72b1156a850f629ea82a1398ca727d44ec1d
Matthew Michilot <matthew.michilot@gmail.com>
2025-09-12 23:13:44 +0000

    Rework oss-cad-suite install process
    tabbypip couldn't install packages because /usr/local is only
    accessible to root. This rework changes detects the user the container
    will run with, changes the ownership of the oss-cad-suite folder, and
    updates the user's bashrc.

--- test/oss-cad-suite/autoActivate.sh
+++ test/oss-cad-suite/autoActivate.sh
@@ -3,5 +3,8 @@
 source dev-container-features-test-lib
 
 check "oss-cad-suite environment is activated" bash -c "[[ '${VIRTUAL_ENV_PROMPT}' == 'OSS CAD Suite' ]]"
+check "oss-cad-suite is user accessible" bash -c "stat -c "%U:%G" ${SUITE_DIR} | grep '${USER}:${USER}'"
+check "tools are accessible" bash -c "verilator --version && yosys --version"
+check "tabbypip can install packages" bash -c "tabbypip install riscof"
 
 reportResults
--- test/oss-cad-suite/test.sh
+++ test/oss-cad-suite/test.sh
@@ -3,6 +3,7 @@
 source dev-container-features-test-lib
 
 check "oss-cad-suite environment is not activated" bash -c "[[ -z '${VIRTUAL_ENV_PROMPT}' ]]"
-check "check activate-suite alias" alias | grep 'activate-suite'
+check "oss-cad-suite is user accessible" bash -c "stat -c "%U:%G" ${SUITE_DIR} | grep '${USER}:${USER}'"
+check "oss-cad-suite environment is sourceable" bash -c "source ${SUITE_DIR}/environment"
 
 reportResults
