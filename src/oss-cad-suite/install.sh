12c282b905386c3302c465ec39b4fea9d1accc76
Matthew Michilot <matthew.michilot@gmail.com>
2025-09-13 11:36:51 +0000

    disable install

--- src/oss-cad-suite/install.sh
+++ src/oss-cad-suite/install.sh
@@ -2,62 +2,63 @@
 
 SUITE_DIR=${SUITE_DIR}
 AUTOACTIVATE=${AUTOACTIVATE}
-USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+# USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
 
 # Determine the appropriate non-root user
 # Copied from https://github.com/devcontainers/features/blob/main/src/anaconda/install.sh
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-        if id -u "${CURRENT_USER}" > /dev/null 2>&1; then
-            USERNAME="${CURRENT_USER}"
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
-    fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
-    USERNAME=root
-fi
-echo "Username: ${USERNAME}"
+# if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+#     USERNAME=""
+#     POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+#     for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+#         if id -u "${CURRENT_USER}" > /dev/null 2>&1; then
+#             USERNAME="${CURRENT_USER}"
+#             break
+#         fi
+#     done
+#     if [ "${USERNAME}" = "" ]; then
+#         USERNAME=root
+#     fi
+# elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+#     USERNAME=root
+# fi
+# USERNAME="$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)"
 
 
-# Download and extract OSS CAD Suite
-archive_url=$(curl -sL https://api.github.com/repos/YosysHQ/oss-cad-suite-build/releases/latest | \
-    jq -r '.assets[] | select(.name | contains("linux-x64")) | .browser_download_url')
+# # Download and extract OSS CAD Suite
+# archive_url=$(curl -sL https://api.github.com/repos/YosysHQ/oss-cad-suite-build/releases/latest | \
+#     jq -r '.assets[] | select(.name | contains("linux-x64")) | .browser_download_url')
 
-wget -qO - "${archive_url}" | tar -xzf - -C "$(dirname ${SUITE_DIR})"
-
-
-# Activate environment, if needed
-bashrc="/home/${USERNAME}/.bashrc"
-source_cmd="source ${SUITE_DIR}/environment"
-
-if [ "${AUTOACTIVATE}" = "true" ]; then
-    echo ${source_cmd} >> "${bashrc}"
-fi
+# wget -qO - "${archive_url}" | tar -xzf - -C "$(dirname ${SUITE_DIR})"
 
 
-# Add notice on how to activate environment
-cat <<'EOF' > "${SUITE_DIR}/activate.txt"
-#
-# To activate the OSS CAD Suite environment manually, use
-#
-#     $ source \${SUITE_DIR}/environment
-#
-# To deactivate, use
-#
-#     $ deactivate
-EOF
+# # Activate environment, if needed
+# bashrc="/home/${USERNAME}/.bashrc"
+# source_cmd="source ${SUITE_DIR}/environment"
 
-echo "cat ${SUITE_DIR}/activate.txt" >> "${bashrc}"
+# if [ "${AUTOACTIVATE}" = "true" ]; then
+#     echo ${source_cmd} >> "${bashrc}"
+# fi
 
 
-# Make the suite directory accessible to user
-chown -R "${USERNAME}:${USERNAME}" "${SUITE_DIR}"
-chmod -R u=rwx,go=rx "${SUITE_DIR}"
-stat -c "%U:%G ${SUITE_DIR}" ${SUITE_DIR}
-echo "User: $(id)"
-cat /etc/passwd
+# # Add notice on how to activate environment
+# cat <<'EOF' > "${SUITE_DIR}/activate.txt"
+# #
+# # To activate the OSS CAD Suite environment manually, use
+# #
+# #     $ source \${SUITE_DIR}/environment
+# #
+# # To deactivate, use
+# #
+# #     $ deactivate
+# EOF
+
+# echo "cat ${SUITE_DIR}/activate.txt" >> "${bashrc}"
+
+
+# # Make the suite directory accessible to user
+# chown -R "${USERNAME}:${USERNAME}" "${SUITE_DIR}"
+# chmod -R u=rwx,go=rx "${SUITE_DIR}"
+# stat -c "%U:%G ${SUITE_DIR}" ${SUITE_DIR}
+# echo "User: $(id)"
+# cat /etc/passwd
+# exit 1
